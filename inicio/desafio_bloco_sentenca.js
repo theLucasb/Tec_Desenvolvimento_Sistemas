95750ae5a10b38421a6c3898e1a455ab27eeac55
theLucasb <lucasrodrigodes@gmail.com>
2025-04-15 22:25:49 -0300

    Inicio com sentenças, blocos e dados

---
+++ inicio/desafio_bloco_sentenca.js
@@ -0,0 +1,23 @@
+/* Desafio: Criar dois blocos de código
+O primeiro deve imprimir na tela os
+números pares de 0 a 10 e o segundo
+bloco deve imprimir na tela os ímpares */
+
+{
+    console.log("Pares: ");
+    console.log(0);
+    console.log(2);
+    console.log(4);
+    console.log(6);
+    console.log(8);
+    console.log(10);
+}
+
+{
+    console.log("Ímpares: ");
+    console.log(1);
+    console.log(3);
+    console.log(5);
+    console.log(7);
+    console.log(9);
+}
