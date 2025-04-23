eeae544f3134b4ea70f9e6f3976bf48187cfda99
theLucasb <lucasrodrigodes@gmail.com>
2025-04-22 22:17:33 -0300

    Desafios área e troca

---
+++ inicio/desafio_area.js
@@ -0,0 +1,7 @@
+const PI = 3.141592;
+const raio = 10;
+let areaCirc = PI * raio * raio;
+
+console.log("O valor da circunferência é: " + areaCirc + "m²");
+
+// PI * raio²
---
+++ inicio/desafio_troca.js
@@ -0,0 +1,13 @@
+// Desafio: Criar um programa que altere o valor de duas variáveis.
+// A primeira variável terá o valor 94 e a segunda terá o valor 7.
+// Imprimir os valores das variáveis na tela.
+
+var a = 94;
+var b = 7;
+
+var temp = a; //94
+a = b; // 7
+b = temp; // 94
+
+console.log(a);
+console.log(b);
