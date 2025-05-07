7310398140a16880adc1fb8c761b9835483161e3
theLucasb <lucasrodrigodes@gmail.com>
2025-05-06 22:12:25 -0300

    Trabalhando com operadores

---
+++ operadores/aritmeticos.js
@@ -0,0 +1,16 @@
+// valores literais
+console.log(2 + 3); // adição
+console.log(2 - 3); // subtração
+console.log(2 * 3); // multiplicação
+console.log(2 / 3); // divisão
+console.log(2 % 3); // resto da divisão
+
+let a = 10;
+let b = 5;
+
+// variaveis
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
---
+++ operadores/desafio_relacionais.js
@@ -0,0 +1,19 @@
+let a = 10;
+let b = 11;
+
+// Imprimir 5 valores verdadeiros (true)
+console.log(a < b);
+console.log(a <= b);
+console.log(a != b);
+console.log(a !== b);
+console.log(b >= a);
+console.log(a == 10);
+console.log(a + 1 == b);
+
+// Imprimir 5 valores falsos (false)
+console.log(a > b);
+console.log(a >= b);
+console.log(a == b);
+console.log(a >= b);
+console.log(a === b);
+
