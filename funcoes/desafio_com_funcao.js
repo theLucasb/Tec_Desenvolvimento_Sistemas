4b8dd3a81feadd466851f41c0f4566dbf2835ebf
theLucasb <lucasrodrigodes@gmail.com>
2025-05-20 21:01:30 -0300

    Intrudução à funções no JS

---
+++ funcoes/desafio_com_funcao.js
@@ -0,0 +1,19 @@
+// Desafio:
+
+// Criar uma função que receba dois 
+// números/parâmetros, imprima na tela a soma os dois.
+
+// Criar uma função que receba dois 
+// números/parâmetros, imprima na tela a subtraia os dois.
+
+function somar(n1, n2) {
+    console.log(n1 + n2);
+}
+
+somar(10, 15);
+
+function subtrair(n1, n2) {
+    console.log(n1 - n2);
+}
+
+subtrair(10, 15);
